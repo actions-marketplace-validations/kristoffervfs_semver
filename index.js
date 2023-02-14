21de577e9d206c65c45fd1b8206534293878394a
Kristoffer <90189578+kristoffervfs@users.noreply.github.com>
2023-02-14 13:45:54 +0100

    refactor(ending): removed a line

--- index.js
+++ index.js
@@ -287,5 +287,4 @@ function getCommitMessage(str){
     throw new Error('Commit message "' + str + '" did not match regex');
 
   return '**' + arr[1].replace(/\(|\)/g,'') + '**, ' + arr[2].replace(/^ /g, '');
-
 }
